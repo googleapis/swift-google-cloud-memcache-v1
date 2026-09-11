a523519647478d1e867d6d5583b340e02b3b95f6
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudMemcacheV1/Instance.swift
+++ Sources/GoogleCloudMemcacheV1/Instance.swift
@@ -306,11 +306,11 @@ public struct Instance: Codable, Equatable, GoogleCloudWKT._AnyPackable,
       public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         switch self {
-        case .unspecified: return try container.encode(0)
-        case .creating: return try container.encode(1)
-        case .ready: return try container.encode(2)
-        case .deleting: return try container.encode(3)
-        case .updating: return try container.encode(4)
+        case .unspecified: return try container.encode("STATE_UNSPECIFIED")
+        case .creating: return try container.encode("CREATING")
+        case .ready: return try container.encode("READY")
+        case .deleting: return try container.encode("DELETING")
+        case .updating: return try container.encode("UPDATING")
         case .unknownIntValue(let v): return try container.encode(v)
         case .unknownStringValue(let v): return try container.encode(v)
         }
@@ -442,8 +442,9 @@ public struct Instance: Codable, Equatable, GoogleCloudWKT._AnyPackable,
       public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         switch self {
-        case .unspecified: return try container.encode(0)
-        case .zoneDistributionUnbalanced: return try container.encode(1)
+        case .unspecified: return try container.encode("CODE_UNSPECIFIED")
+        case .zoneDistributionUnbalanced:
+          return try container.encode("ZONE_DISTRIBUTION_UNBALANCED")
         case .unknownIntValue(let v): return try container.encode(v)
         case .unknownStringValue(let v): return try container.encode(v)
         }
@@ -576,12 +577,12 @@ public struct Instance: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .creating: return try container.encode(1)
-      case .ready: return try container.encode(2)
-      case .updating: return try container.encode(3)
-      case .deleting: return try container.encode(4)
-      case .performingMaintenance: return try container.encode(5)
+      case .unspecified: return try container.encode("STATE_UNSPECIFIED")
+      case .creating: return try container.encode("CREATING")
+      case .ready: return try container.encode("READY")
+      case .updating: return try container.encode("UPDATING")
+      case .deleting: return try container.encode("DELETING")
+      case .performingMaintenance: return try container.encode("PERFORMING_MAINTENANCE")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
--- Sources/GoogleCloudMemcacheV1/MemcacheVersion.swift
+++ Sources/GoogleCloudMemcacheV1/MemcacheVersion.swift
@@ -105,8 +105,8 @@ public enum MemcacheVersion: Codable, Equatable, Sendable {
   public func encode(to encoder: Encoder) throws {
     var container = encoder.singleValueContainer()
     switch self {
-    case .unspecified: return try container.encode(0)
-    case .memcache15: return try container.encode(1)
+    case .unspecified: return try container.encode("MEMCACHE_VERSION_UNSPECIFIED")
+    case .memcache15: return try container.encode("MEMCACHE_1_5")
     case .unknownIntValue(let v): return try container.encode(v)
     case .unknownStringValue(let v): return try container.encode(v)
     }
